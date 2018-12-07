d1bac9698b1e30095718143ade93facfad73ff8e
Gregory Salinas <asalinas720@gmail.com>
2018-12-07 15:13:01 -0800

    Navbar functionality for mobile works in desktop, not in remote device

--- assets/js/components/navbar.js
+++ assets/js/components/navbar.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
+    toggleDropdownMobile();
+
     // Occur only in index page
     if(document.location.pathname === '/') {
         navBrandAnimation('logo-animation', 2500, 400);
@@ -13,11 +15,15 @@ function navBrandAnimation(classSelector, timer, nestedTimer) {
     for (var i = 0; i <= $navLogos.length; i++) {
         var innerVar = 0;
         (function() {
+            if(typeof $navLogos[i] === 'undefined') {
+                return;
+            }
+
             var el = $navLogos[i];
 
             // Display and Flip Brand Logo
             setTimeout(function() {
-                el.style.display = 'inline-block';
+                el.style.display = 'inherit';
                 el.className += ' flip-in';
 
                 // Flip Out Brand Logo
@@ -40,9 +46,31 @@ function navBrandAnimation(classSelector, timer, nestedTimer) {
 function mainNavBrandAnimation(classSelector) {
     var $navLogos = document.getElementsByClassName(classSelector);
 
-    for (var i = 0; i <= $navLogos.length; i++) {
-        var el = $navLogos[i];
-        el.style.display = 'inline-block';
+    for (var el of $navLogos) {
+        el.style.display = 'inherit';
         el.className += ' flip-in';
     }
+}
+
+function toggleDropdownMobile() {
+    if(window.innerWidth >= 768) {
+        return;
+    }
+
+    var $dropDown = document.getElementsByClassName('nav-dropdown');
+
+    for(var el of $dropDown) {
+        var $dropDownLink = document.getElementsByClassName(el.classList[1])[0];
+        listenFor($dropDownLink);
+    }
+}
+
+function listenFor(el) {
+    el.addEventListener('click', function(){
+        if (el.classList.contains('active')) {
+            el.className = el.className.replace(/\bactive\b/g, '');
+        } else {
+            el.className += ' active';
+        }
+    });
 }
