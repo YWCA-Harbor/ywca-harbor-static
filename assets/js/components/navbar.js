99a86b01f5342673ea5df028aa6071ef8abc9917
Gregory Salinas <asalinas720@gmail.com>
2018-12-09 18:37:29 -0800

    Minor changes to navbar.js and files for animation and home page

--- assets/js/components/navbar.js
+++ assets/js/components/navbar.js
@@ -14,39 +14,40 @@ function navBrandAnimation(classSelector, timer, nestedTimer) {
 
     for (var i = 0; i <= $navLogos.length; i++) {
         var innerVar = 0;
-        (function() {
-            if(typeof $navLogos[i] === 'undefined') {
+
+        // Display and Flip Brand Logo
+        setTimeout((function(j) {
+            // Edge case
+            if(typeof $navLogos[j] === 'undefined') {
                 return;
             }
 
-            var el = $navLogos[i];
+            var el = $navLogos[j];
+            el.style.display = 'inherit';
+            el.className += ' flip-in';
 
-            // Display and Flip Brand Logo
+            // Flip Out Brand Logo
             setTimeout(function() {
-                el.style.display = 'inherit';
-                el.className += ' flip-in';
-
-                // Flip Out Brand Logo
+                el.className += ' flip-out';
+                //  and Hide
                 setTimeout(function() {
-                    el.className += ' flip-out';
-                    //  and Hide
-                    setTimeout(function() {
-                        innerVar++;
-                        el.style = '';
-                        el.className = classSelector;
-                        innerVar === $navLogos.length ?
-                        mainNavBrandAnimation('main-logo-animation') : 'Don\'t do anything';
-                    }, nestedTimer * i);
+                    innerVar++;
+                    el.style = '';
+                    el.className = classSelector;
+                    if(innerVar === $navLogos.length) {
+                        mainNavBrandAnimation('main-logo-animation');
+                    }
                 }, nestedTimer * i);
-            }, timer * i);
-        })(i);
+            }, nestedTimer * i);
+        }).bind(null, i), timer * i);
     }
 }
 
 function mainNavBrandAnimation(classSelector) {
     var $navLogos = document.getElementsByClassName(classSelector);
 
-    for (var el of $navLogos) {
+    for (var i = 0; i < $navLogos.length; i++) {
+        var el = $navLogos[i];
         el.style.display = 'inherit';
         el.className += ' flip-in';
     }
@@ -59,7 +60,8 @@ function toggleDropdownMobile() {
 
     var $dropDown = document.getElementsByClassName('nav-dropdown');
 
-    for(var el of $dropDown) {
+    for(var i = 0; i < $dropDown.length; i++) {
+        var el = $dropDown[i];
         var $dropDownLink = document.getElementsByClassName(el.classList[1])[0];
         listenFor($dropDownLink);
     }
@@ -67,6 +69,16 @@ function toggleDropdownMobile() {
 
 function listenFor(el) {
     el.addEventListener('click', function(){
+        var $activeEl = document.getElementsByClassName('nav-dropdown active');
+
+        for(var i = 0; i < $activeEl.length; i++) {
+            var $active = $activeEl[i];
+
+            if($active !== el) {
+                $active.className = $active.className.replace(/\bactive\b/g, '');
+            }
+        }
+
         if (el.classList.contains('active')) {
             el.className = el.className.replace(/\bactive\b/g, '');
         } else {
